e3647117d8dfff41c1fc86b53ee398f983eaa4b3
Sean <xujihui1985@gmail.com>
2021-07-10 17:48:06 +0000

    implement inspect

--- provider/src/connector/docker.rs
+++ provider/src/connector/docker.rs
@@ -1,7 +1,35 @@
-use hyper::{Client};
+use hyper::{body::Buf, Client};
 use hyperlocal::{UnixClientExt, UnixConnector, Uri};
+use serde::{Deserialize, Serialize};
 
 const INFO_PATH: &'static str = "/info";
+const LIST_CONTAINERS_PATH: &'static str = "/containers/json";
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Container {
+    #[serde(rename = "Id")]
+    pub id: String,
+    #[serde(rename = "Names")]
+    pub names: Vec<String>,
+    #[serde(rename = "State")]
+    pub state: String,
+    #[serde(rename = "Status")]
+    pub status: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ContainerDetail {
+    #[serde(rename = "Id")]
+    pub id: String,
+    #[serde(rename = "State")]
+    pub state: ContainerState,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+pub struct ContainerState {
+    #[serde(rename = "Pid")]
+    pub pid: u64,
+}
 
 pub struct DockerClient {
     sock_path: String,
@@ -18,12 +46,32 @@ impl DockerClient {
         client
     }
 
-    pub fn list_containers(&self) {}
+    pub async fn list_containers(&self) {
+        let url = Uri::new(&self.sock_path, LIST_CONTAINERS_PATH).into();
+        let response = self.client.get(url).await.unwrap();
+        let status = response.status();
+        if status.is_success() {
+            let body = hyper::body::aggregate(response).await.unwrap();
+            let containers: Vec<Container> = serde_json::from_reader(body.reader()).unwrap();
+            println!("{:?}", containers);
+        }
+    }
 
     pub async fn info(&self) -> bool {
         let url = Uri::new(&self.sock_path, INFO_PATH).into();
         let response = self.client.get(url).await.unwrap();
         let status = response.status();
         status.is_success()
+    }
+
+    pub async fn inspect(&self, id: impl Into<String>) {
+        let url = Uri::new(&self.sock_path, &format!("/containers/{}/json", id.into())).into();
+        let response = self.client.get(url).await.unwrap();
+        let status = response.status();
+        if status.is_success() {
+            let body = hyper::body::aggregate(response).await.unwrap();
+            let detail: ContainerDetail = serde_json::from_reader(body.reader()).unwrap();
+            println!("{:?}", detail);
+        }
     }
 }
--- src/bin/mario/main.rs
+++ src/bin/mario/main.rs
@@ -3,12 +3,9 @@ use provider::connector::docker::DockerClient;
 #[tokio::main]
 async fn main() {
     println!("before connect");
-    std::thread::sleep(std::time::Duration::from_secs(30));
     {
         let client = DockerClient::connect("/var/run/docker.sock");
-        client.info().await;
-        std::thread::sleep(std::time::Duration::from_secs(60));
+        client.inspect("ed8f5b69e107").await;
     }
     println!("after connect");
-    std::thread::sleep(std::time::Duration::from_secs(60));
 }
