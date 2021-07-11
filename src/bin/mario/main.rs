d04e263dc4bb7542ce1e1e31a1c2b30b08cb90f5
Sean <xujihui1985@gmail.com>
2021-07-11 00:06:02 +0000

    collect cpu status

--- src/bin/mario/main.rs
+++ src/bin/mario/main.rs
@@ -1,5 +1,8 @@
 use std::collections::HashMap;
-use tokio::sync::oneshot;
+use tokio::{
+    io::{AsyncBufReadExt, BufReader},
+    sync::oneshot,
+};
 
 use provider::connector::{
     docker::DockerClient,
@@ -23,12 +26,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             }
         }
     });
+    let collect_handler = tokio::spawn(collect());
     let timeout_handle = tokio::spawn(async move {
         tokio::time::sleep(std::time::Duration::from_secs(12)).await;
         tx.send(()).unwrap();
     });
     watch_container_handler.await?;
     timeout_handle.await?;
+    collect_handler.await?;
     Ok(())
 }
 
@@ -59,10 +64,30 @@ async fn task_inner(client: &DockerClient) {
     }
 }
 
-async fn collect(containers: HashMap<String, Container>) {
+async fn collect() {
     let mut interval = tokio::time::interval(std::time::Duration::from_secs(5));
     loop {
         interval.tick().await;
-        containers.iter().for_each(|(id, c)| {});
+        let proc_stat = tokio::fs::File::open("/proc/stat").await.unwrap();
+        let buf_reader = BufReader::new(proc_stat);
+        let mut lines = buf_reader.lines();
+        while let Some(line) = lines.next_line().await.unwrap() {
+            if line.starts_with("cpu ") {
+                let part = line[5..].split(" ").collect::<Vec<_>>();
+                println!("usr {}, nice {}, sys {}, idle {}, iowait {}, hardirq {}, softirq {}, steal {}, guest {}", part[0], part[1], part[2], part[3], part[4], part[5], part[6], part[7], part[8]);
+                break;
+            }
+        }
+
+        let cpuinfo = tokio::fs::File::open("/proc/cpuinfo").await.unwrap();
+        let buf_reader = BufReader::new(cpuinfo);
+        let mut lines = buf_reader.lines();
+        let mut number_of_cpus = 0;
+        while let Some(line) = lines.next_line().await.unwrap() {
+            if line.starts_with("processor\t:") {
+                number_of_cpus = number_of_cpus + 1;
+            }
+        }
+        println!("number of cpu {}", number_of_cpus);
     }
 }
